43ff2fbf47254bf5a96cc825de0db2def6b804f1
bennettshaeffer <36181162+bennettshaeffer@users.noreply.github.com>
2018-03-06 20:50:40 -0800

    Rename wrong-turn.ino.ino to wrong-turn.ino

---
+++ hw7/wrong-turn.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+
+  //if you take a wrong turn this light turns on, probably the safest way of telling a driver they messed up since flashing lights might be a bit hazardous
+
+  //The code is boring but then again saftey generally is boring
+
+}

37f14bde2c41c571d80626a46cc71b94e1e212fb
bennettshaeffer <36181162+bennettshaeffer@users.noreply.github.com>
2018-03-06 20:50:08 -0800

    Rename overdue.ino.ino to overdue.ino

---
+++ hw7/overdue.ino
@@ -0,0 +1,38 @@
+/*
+ * Super fake "low frequency oscillator" modulation on the LEDPIN
+ * 
+ * I know it does not produce a square wave, its just a fragment from a past idea (also it technically produces square waves so whatever)
+ */
+
+int led = 13;
+int squareLFO = 1000;
+int dir = 0;
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(squareLFO);
+  digitalWrite(13,LOW);
+  delay(squareLFO);
+  
+  if(dir == 0)
+  {
+    squareLFO = squareLFO - 100;
+    if(squareLFO == 0)
+    {
+      dir = 1;
+      squareLFO = 100;
+    }
+  }
+  else
+  {
+    squareLFO = squareLFO + 100;
+    if(squareLFO == 1000)
+    {
+      dir = 0;
+      squareLFO = 900;
+    }
+  }
+}
